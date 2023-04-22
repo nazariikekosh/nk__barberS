37d47def8f27408e4fd11922d4c7066f81c38207
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-04-22 09:40:46 +0300

    add__burger-menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,38 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+// let slider = $(document).ready(function () {
+//   $('.your-class').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     arrows: true,
+//     dots: true,
+//     infinite: true
+//   });
+// });
 
+
+// -----------burger----------
+
+function burgerMenu(selector){
+  let menu = $(selector);
+  let button = menu.find('.burger-menu__button');
+  let links = menu.find('.burger-menu__link');
+  let overlay =  menu.find('.burger-menu__overlay')
+  button.on('click',(e) => {
+    e.preventDefault();
+    toggleMenu();
+  })
+  links.on('click', () => toggleMenu());
+  overlay.on('click', () => toggleMenu());
+
+  function toggleMenu(){
+    menu.toggleClass('burger-menu_active');
+
+    if(menu.hasClass('burger-menu_active')) {
+      $('body').css('overflow','hidden');
+    } else {
+      $('body').css('overflow','visible');
+    }
+  }
+}
+burgerMenu('.burger-menu');
+
+// ------------------------------
